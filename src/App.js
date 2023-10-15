0945e563bccc1cce7890ae367dd92891984b23d4
Jeanne Allen <tebazele@gmail.com>
2023-10-15 13:45:56 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import * as echarts from 'echarts';
 
 function App() {
+  let myChart = echarts.init(document.getElementById('main'))
+  myChart.setOption({
+    title: {
+      text: 'Heatmap example'
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'heatmap'
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div id="main" style="width: 600px; height:400px;"></div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
